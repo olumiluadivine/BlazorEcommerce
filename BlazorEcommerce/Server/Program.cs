e6931db1105aa5564759568834992cbf79af9a10
Divine Favour <favourme2580@gmail.com>
2022-08-21 11:55:53 +0100

    Db Added

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,10 +1,19 @@
 global using BlazorEcommerce.Shared;
+global using Microsoft.EntityFrameworkCore;
+using BlazorEcommerce.Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
