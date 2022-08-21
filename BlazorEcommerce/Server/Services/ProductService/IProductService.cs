dcbd2c73d49726551efac4202f9ae7a6d4a41738
Divine Favour <favourme2580@gmail.com>
2022-08-21 20:56:43 +0100

    Added another endpoint and called it

--- BlazorEcommerce/Server/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -2,6 +2,7 @@
 {
     public interface IProductService
     {
-        Task<ServiceResponse<List<Product>>> GetProductAsync();
+        Task<ServiceResponse<List<Product>>> GetProductsAsync();
+        Task<ServiceResponse<Product>> GetProductAsync(int productId);
     }
 }
