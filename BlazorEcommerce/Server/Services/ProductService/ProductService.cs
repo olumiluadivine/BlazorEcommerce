746c8d0627160ed88b4013fbc8551f1ac0afa7fe
Divine Favour <favourme2580@gmail.com>
2022-08-22 11:52:16 +0100

    Search API

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -48,5 +48,18 @@
 
             return response;
         }
+
+        public async Task<ServiceResponse<List<Product>>> SearchProducts(string searchText)
+        {
+            var response = new ServiceResponse<List<Product>>()
+            {
+                Data = await _context.Products
+                .Where(p=>p.Title.ToLower().Contains(searchText.ToLower())
+                || p.Description.ToLower().Contains(searchText.ToLower()))
+                .Include(p=>p.Variants).ToListAsync()
+            };
+
+            return response;
+        }
     }
 }
